7acb56bc1b2a625d564d2332d7b16153a2bd460c
Stanisław Górka <stanislaw.gorka.sg@gmail.com>
2024-09-27 12:49:23 +0200

    Added .hpp and .cpp files for GameMode class

---
+++ GameMode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by metre on 27.09.2024.
+//
+
+#include "GameMode.hpp"
---
+++ GameMode.hpp
@@ -0,0 +1,19 @@
+//
+// Created by metre on 27.09.2024.
+//
+
+#pragma once
+
+
+#include "Player.hpp"
+
+class GameMode {
+private:
+    Player user, opponent;
+
+public:
+
+};
+
+
+
